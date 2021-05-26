2f586827c835714dd81c7d4c9919e9d4b0f77406
steel-hyuk <skypeed3@naver.com>
2021-05-26 22:13:20 +0900

    week1 corrected assignment

--- KimEunHyuk/week1/2739_multiplicationTable.js
+++ KimEunHyuk/week1/2739_multiplicationTable.js
@@ -3,7 +3,10 @@ const input =  fs.readFileSync('/dev/stdin').toString().split(' ');
 
 let num = Number(input);
 
+// num을 인자로 넣으면 구구단의 num단을 출력
 function doMultiply(num){
+
+  // 반복하면서 num * i = (num*i) 형태로 출력
   for (let i=1; i<10; i++) {
     console.log(`${num} * ${i} = ${num*i}`);
   }
--- KimEunHyuk/week1/2753_leapYear.js
+++ KimEunHyuk/week1/2753_leapYear.js
@@ -5,12 +5,15 @@ let year = Number(input);
 
 function isLeapYear(year){
   let bool = 0;
+  // 4의 배수인 경우 윤년
   if (year % 4 === 0) {
     bool = 1;
   }
+  // 100의 배수인 경우 윤년 x
   if (year % 100 === 0) {
     bool = 0;
   }
+  // 400의 배수인 경우 윤년
   if (year % 400 === 0) {
     bool = 1;
   }
