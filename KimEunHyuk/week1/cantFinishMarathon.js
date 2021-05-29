d1a5d12694828a5e82821827e6e956059e519820
steel-hyuk <skypeed3@naver.com>
2021-05-29 13:22:22 +0900

    05.29 success

--- KimEunHyuk/week1/cantFinishMarathon.js
+++ KimEunHyuk/week1/cantFinishMarathon.js
@@ -1,20 +1,7 @@
-function sort(array){
-    for (let i=0; i<array.length-1;i++){
-        for (let j=i+1; j<array.length;j++){
-            if (array[j] < array[i]){
-                let tmp = array[j];
-                array[j] = array[i];
-                array[i] = tmp;
-            }
-        }
-    }
-    return array;
-}
-
 function solution(participant, completion) {
     let answer = '';
-    let part = sort(participant);
-    let comp = sort(completion);
+    let part = participant.sort();
+    let comp = completion.sort();
     
     for (let i=0; i<part.length; i++) {
         if (part[i] !== comp[i]){
