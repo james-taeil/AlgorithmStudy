f5d4418659642f5c072a9fd8967bbfee9023b38f
steel-hyuk <59719552+steel-hyuk@users.noreply.github.com>
2021-06-12 17:47:26 +0900

    0612

--- KimEunHyuk/week3/2016.js
+++ KimEunHyuk/week3/2016.js
@@ -1,3 +1,17 @@
-function a() {
-  return a;
+function solution(n, lost, reserve) {
+   let answer = 0;
+   let minus = lost.length;
+
+   for (let i=0; i<lost.length; i++){
+       for (let j=0; j<reserve.length; j++){
+           if (lost[i] === reserve[j]-1 || lost[i] === reserve[j]+1){
+               reserve.splice(j--,1);
+               minus--;
+               break;
+           }
+       }
+   }
+   answer = n - minus;
+
+   return answer;
 }
