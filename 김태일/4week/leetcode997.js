72013f6ec62671aff074d39c43ef1f234961920e
edan_3000 <roseswallow13@gmail.com>
2021-06-27 19:27:11 +0900

    에러케이스 1개 존재

--- 김태일/4week/leetcode997.js
+++ 김태일/4week/leetcode997.js
@@ -1,21 +1,21 @@
 function findJudge (n, trust) {
     const matrix = new Array(n).fill(0).map(() => Array(n).fill(0));
-    const find = new Array(n).fill(0)
+    const find = new Array(n).fill(0);
     // 값 넣어주기
     for(let [i, j] of trust) {
-        matrix[i-1][j-1] = 1
+        matrix[i-1][j-1] = 1;
     }
 
     // 반복문으로 row 행에 1이 없는 인덱스 찾기
     for(let i = 0; i < matrix.length; i++) {
-        find[i] = matrix[i].reduce((a, b) => a + b, 0)
+        find[i] = matrix[i].reduce((a, b) => a + b, 0);
     }
 
-    if (find.indexOf(0)) {
-        return find.indexOf(0) + 1
+    if (find.indexOf(0) !== -1) {
+        return find.indexOf(0) + 1;
+    } else {
+        return -1;
     }
-
-    return -1;
 }
 
-console.log(findJudge(4, [[1,3],[1,4],[2,3],[2,4],[4,3]]))
+console.log(findJudge(3, [[1,3],[2,3],[3,1]]))
