f5afc2de199dc84e8bd0da7db163924794cd1558
steel-hyuk <skypeed3@naver.com>
2021-07-03 00:29:32 +0900

    unsolved hIndex

--- KimEunHyuk/week6/hIndex.js
+++ KimEunHyuk/week6/hIndex.js
@@ -1,8 +1,8 @@
 function solution(citations) {
     var answer = 0;
-    
     citations.sort((a,b) => b-a);
-
+    
+    
     for (let el of citations){
         if (el >= answer && el !== 0)
             answer++;
@@ -11,5 +11,22 @@ function solution(citations) {
         }
     }
     // 1 1 0 을 입력받았을 때, 1을 리턴해야하는데 2를 리턴하고 있음.
+    
+    // 2 2 0 을 입력받으면, 2리턴 ㅇㅋ
+    
+    // 2 2 2 를 입력받으면, 3리턴 x
+    
+    // 3 3 0 => 2리턴 ㅇㅋ
+    
+    // 7 7 3 1 1
+    
+    // 3 2 2 => 3리턴 x
+    
+    // 앞의 요소와 같은 경우를 count ? 
+    
+    // 아예 큰 경우 [5, 5, 0] 이런 경우에는 해당 안되기 때문에 불편
+    
+    // 같은 것이 여러 개 있을 경우는 ? [3 3 2 2 0] count를 배열 형태? 객체 형태로? 상당히 골치아플 듯
+    
     return answer;
 }
