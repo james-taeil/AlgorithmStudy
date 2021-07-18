0761da91fb866715ea218c61cde51314fe519349
SiwooPak <bogus1620@naver.com>
2021-07-18 18:44:00 +0900

    Update 42842.js

--- ParkSiwoo/week01/week07/42842.js
+++ ParkSiwoo/week01/week07/42842.js
@@ -6,7 +6,7 @@ function solution(brown, yellow) {
     for(let i=3; i<total/2; i++) {
         let len = total/i;
 
-        if((i-2)*(len-2) === y) {
+        if((i-2)*(len-2) === yellow) {
             let weight = Math.max(len,i);
             let height = Math.min(len,i);
 
