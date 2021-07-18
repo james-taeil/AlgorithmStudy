1cc6b903fcc1b9f09c643739403bf21595394847
SiwooPak <bogus1620@naver.com>
2021-07-18 20:02:16 +0900

    Update 42840.js

--- ParkSiwoo/week01/week07/42840.js
+++ ParkSiwoo/week01/week07/42840.js
@@ -1,33 +1,28 @@
-// 모의고사
 function solution(answers) {
-    const answer = [
+    // 수포자들의 답안패턴
+    const students = [
         [1,2,3,4,5],
         [2,1,2,3,2,4,2,5],
         [3,3,1,1,2,2,4,4,5,5]
     ];
-    const cnt = [0,0,0];
+    // 수포자들의 점수
+    let score = [];
 
-    for(let i=0; i<answers.length; i++) {
-        if(answer[0][i%5] === answer[i]) {
-            cnt[0]++;
-        }
-        if(answer[i][i%8] === answer[i]) {
-            cnt[1]++;
-        }
-        if(answer[i][i%10] === answer[i]) {
-            cnt[2]++;
-        }
+    // 수포자별 점수 계산
+    for(let student of students) {
+        
+        score.push(answers.reduce((acc,cur,idx) => {
+            // 해당 인덱스의 정답이 수포자의 정답 패턴에 일치할경우 점수증가
+            cur === student[idx%student.length] ? acc++ : acc;
+            // 해당 수포자의 점수를 score 에 push
+            return acc;
+        },0))
     }
 
-    let max = 0;
-    let result = [];
-    for(let j=0; j<cnt.length; j++) {
-        if(cnt[j]>max) max=cnt[j]
-    }
-
-    for(let k=0; k<cnt.length; k++) {
-        if(max === cnt[k]) result.push(k+1);
-    }
-
-    return result.sort();
+    // 가장 많은 문제를 맞힌 사람 return
+    return score.reduce((acc,cur,idx) => {
+        // 현재 수포자의 점수가 최대점수와 같을 경우, idx+1값을 배열에 push
+        cur === Math.max(...score) ? acc.push(idx+1): acc;
+        return acc;
+    },[])
 }
