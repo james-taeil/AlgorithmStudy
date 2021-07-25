1f0ebc302c74c56eae821ce983c9468c50919af6
edan_3000 <roseswallow13@gmail.com>
2021-07-25 19:33:51 +0900

    8주차 과제

--- 김태일/8week/큰 수 만들기.js
+++ 김태일/8week/큰 수 만들기.js
@@ -1,29 +1,47 @@
 function solution(number, k) {
-    const getCombinations = function (arr, selectNumber) {
-        const results = [];
-        if (selectNumber === 1) return arr.map((value) => [value]); 
+    // const getCombinations = function (arr, selectNumber) {
+    //     const results = [];
+    //     if (selectNumber === 1) return arr.map((value) => [value]); 
       
-        arr.forEach((fixed, index, origin) => {
-          const rest = origin.slice(index + 1); 
-          const combinations = getCombinations(rest, selectNumber - 1);
-          const attached = combinations.map((combination) => [fixed, ...combination]); 
-          results.push(...attached);
-        });
+    //     arr.forEach((fixed, index, origin) => {
+    //       const rest = origin.slice(index + 1); 
+    //       const combinations = getCombinations(rest, selectNumber - 1);
+    //       const attached = combinations.map((combination) => [fixed, ...combination]); 
+    //       results.push(...attached);
+    //     });
       
-        return results;
-      }
-    let arr = number.split('');
-    let result = []
-    let max = 0;
-    let selectNum = number.length - k
-    arr = getCombinations(arr, selectNum);
-    for (let i = 0; i < arr.length; i++) {
-        result.push(Number(arr[i].join('')))
+    //     return results;
+    //   }
+    // let arr = number.split('');
+    // let result = []
+    // let max = 0;
+    // let selectNum = number.length - k
+    // arr = getCombinations(arr, selectNum);
+    // for (let i = 0; i < arr.length; i++) {
+    //     result.push(Number(arr[i].join('')))
+    // }
+    // for (let i = 1; i < result.length; i++) {
+    //     max = Math.max(result[i], result[i - 1]);
+    // }
+    // console.log(result)
+    // return String(max);
+
+    const stack = [];
+    let answer = '';
+
+    for (let i = 0; i < number.length; i++) {
+        const el = number[i];
+
+        while (k > 0 && stack[stack.length - 1] < el) {
+            stack.pop();
+            k--;
+        }
+        stack.push(el);
     }
-    for (let i = 1; i < result.length; i++) {
-        max = Math.max(result[i], result[i - 1]);
-    }
-    return String(max);
+
+    stack.splice(stack.length-k, k);
+    answer  = stack.join('');
+    return answer;
 }
 
 let number = "4177252841"
